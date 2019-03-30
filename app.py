5a104285c3309591754dd6f9a85130615e7ee982
xueqing <xueqing@foundersc.com>
2019-03-30 21:40:34 +0800

    reback

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__, template_folder='../templates', static_folder="", static_url_path="")
-
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
